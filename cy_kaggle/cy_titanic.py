df1828b086e4b6c83f26ffde53d1ade5a75fcee3
nali <ethan.cheng@ximalaya.com>
2018-02-25 10:11:51 +0800

    备份

--- cy_kaggle/cy_titanic.py
+++ cy_kaggle/cy_titanic.py
@@ -1,6 +1,7 @@
 import csv
 
 import numpy as np
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import Imputer
 
 
@@ -58,3 +59,42 @@ def auto_norm(dataset):
     # 再除以范围获得归一化矩阵
     normDataSet = normDataSet / np.tile(range_val, (m, 1))
     return normDataSet, range_val, min_val
+
+
+def set_missing_ages(df):
+    """
+    使用随机森林预测缺失的年龄
+    :param df:
+    :return:
+    """
+
+    # 把已有的数值型特征取出来丢进Random Forest Regressor中
+    age_df = df[['Age', 'Fare', 'Parch', 'SibSp', 'Pclass']]
+
+    # 乘客分成已知年龄和未知年龄两部分
+    known_age = age_df[age_df.Age.notnull()].as_matrix()
+    unknown_age = age_df[age_df.Age.isnull()].as_matrix()
+
+    # y即目标年龄
+    y = known_age[:, 0]
+
+    # X即特征属性值
+    X = known_age[:, 1:]
+
+    # fit到RandomForestRegressor之中
+    rfr = RandomForestRegressor(random_state=0, n_estimators=2000, n_jobs=-1)
+    rfr.fit(X, y)
+
+    # 用得到的模型进行未知年龄结果预测
+    predictedAges = rfr.predict(unknown_age[:, 1::])
+
+    # 用得到的预测结果填补原缺失数据
+    df.loc[(df.Age.isnull()), 'Age'] = predictedAges
+
+    return df, rfr
+
+
+def set_Cabin_type(df):
+    df.loc[(df.Cabin.notnull()), 'Cabin'] = "Yes"
+    df.loc[(df.Cabin.isnull()), 'Cabin'] = "No"
+    return df
